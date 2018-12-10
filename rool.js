a67cafbe481fba0c96b2d5b81fe5fd46992241b9
yuenwailok <yuenwailok11@gmail.com>
2018-12-11 02:44:39 +1100

    Add files via upload

--- rool.js
+++ rool.js
@@ -1,19 +1,15 @@
 'use strict'
 
-$( document ).ready(function() {
-	$(window).scroll(function() {
+$( document ).ready(function() { 	  
+	/*$(window).scroll(function() {
 	if ($(window).scrollTop() >= ($("#Thirdsection").offset().top))
 	{
-		console.log(document.getElementById("robotface").getBBox());
-
 		$("#firstthird").fadeTo(1000,1);
 		$("#secondthird").delay(800).fadeTo(1000,1);
 		$("#thirdthird").delay(1600).fadeTo(1000,1);
 		
-	}
-	//window.requestAnimationFrame(step);
-
-	
+	}*/
+	//window.requestAnimationFrame(step)	
 	//$("changewidth").css("width",$("changewidth").height() * 0.95);
 });
 });
@@ -26,13 +22,6 @@ function myF()
 		behavior: 'smooth'
 	})
 }
-
-window.scroll({
-  top: 100,
-  left: 100,
-  behavior: 'smooth'
-});
-
 //https://stackoverflow.com/questions/379900/fade-in-each-element-one-after-another
 //https://stackoverflow.com/questions/4655273/jquery-window-scrolltop-but-no-window-scrollbottom
 //https://editor.method.ac/  <!-- Created with Method Draw - http://github.com/duopixel/Method-Draw/ -->
